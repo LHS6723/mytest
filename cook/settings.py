89d9be4c36b83371f4e2c5b82802e28f7e284f5c
lhs6723 <lhs6723@gmail.com>
2026-03-16 15:58:27 +0900

    second update

--- cook/settings.py
+++ cook/settings.py
@@ -73,7 +73,12 @@ WSGI_APPLICATION = 'cook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = ""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
