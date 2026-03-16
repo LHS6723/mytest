55bbb7a17edcfb02506ab83bac5e016ea8806304
lhs6723 <lhs6723@gmail.com>
2026-03-16 14:59:59 +0900

    edit two

--- cook/settings.py
+++ cook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kvjn-#1e(ayxjfdl%0py4=7h48py@*v5eiw-gtkwo$44i=_hu9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['LHS4022.pythonanywhere.com']
+ALLOWED_HOSTS = ['LHS4022.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
